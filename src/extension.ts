43fac42f8b7019df04bb70c44b593546cf05e080
lexmin0412 <zhangle_dev@outlook.com>
2023-04-05 00:59:23 +0800

    feat: show current git user on status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { execSync } from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let myStatusBarItem: vscode.StatusBarItem;
+const EVENTS = {
+	use: 'gcm-vscode.use'
+};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gcm-vscode" is now active!');
+// 初始化状态栏
+const initStatusBar = async (context: vscode.ExtensionContext) => {
+	const { subscriptions } = context;
+	const userName = await execSync('git config user.name').toString().trim();
+	const userEmail = await execSync('git config user.email').toString().trim();
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 300);
+	myStatusBarItem.command = EVENTS.use;  // 点击时执行命令
+	myStatusBarItem.text = `${userName}, ${userEmail}`;
+	subscriptions.push(myStatusBarItem);
+	myStatusBarItem.show();
+};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gcm-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gcm-vscode!');
+// 注册事件
+const registerCommand = (context: vscode.ExtensionContext) => {
+	let disposable = vscode.commands.registerCommand(EVENTS.use, async () => {
+		vscode.window.showInformationMessage('gcm use 执行成功!');
 	});
-
 	context.subscriptions.push(disposable);
+};
+
+// 扩展激活
+export async function activate(context: vscode.ExtensionContext) {
+	registerCommand(context);
+	await initStatusBar(context);
 }
 
-// This method is called when your extension is deactivated
+// 扩展销毁
 export function deactivate() {}
